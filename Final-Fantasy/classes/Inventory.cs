d68b6d9fb61370a4f0f621cabad4ab75d2374d0a
Red-H-Star <92303954+Red-H-Star@users.noreply.github.com>
2023-01-31 18:08:22 +0100

    Travail sur Quest.cs
    Définition de méthode de la classe Quest, création des classe Menu & Inventory, rajout d'instances dans creature, item, quest et skill.json.

---
+++ Final-Fantasy/classes/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Fantasy
+{ 
+    internal class Inventory : Menu
+    {
+        private List<Item> _items;
+
+        public Inventory()
+        {
+            _items = new List<Item>();
+        }
+
+        public List<Item> Items { get { return _items; } }
+    }
+}
---
+++ Final-Fantasy/classes/Quest.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Fantasy
+{
+    internal class Quest : Menu
+    {
+        private float _questid;
+        private string _questname;
+        private string _questtype;
+        private string _questpriority;
+        private string _queststatus;
+        private string _questdescription;
+        private string[] _queststep;
+        private string _questadvance;
+        private int _gilreward;
+        private int _xpreward;
+        private Item? _itemreward;
+
+        public Quest(float id, string name, string type, string priority, string status, string description, string[] step, int gil, int xp, Item item)
+        {
+            _questid = id;
+            _questname = name;
+            _questtype = type;
+            _questpriority = priority;
+            _queststatus = status;
+            _questdescription = description;
+            _queststep = step;
+            _questadvance = _queststep[0];
+            _gilreward = gil;
+            _xpreward = xp;
+            _itemreward = item;
+        }
+
+        public float Questid { get { return _questid; } }
+        public string Questname { get { return _questname; } }
+        public string QuestType { get { return _questtype; } }
+        public string QuestStatus { get { return _queststatus; } }
+        public string QuestPriority { get { return _questpriority; } }
+        public string QuestDescription { get { return _questdescription; } }
+        public string[] QuestStep { get { return _queststep;} }
+        public int GilReward { get { return _gilreward;} }
+        public int XpReward { get { return _xpreward;} }
+        public Item? ItemReward { get { return _itemreward;} }
+
+        public void QuestStart()
+        {
+           if(this._queststatus == "Not discovered")
+            {
+                this._queststatus = "Accepted";
+                Console.WriteLine("You have started a new quest !");
+            }
+        }
+
+        public void QuestAdvancement()
+        {
+            int index = Array.IndexOf(this._queststep, this._questadvance);
+            switch (index)
+            {
+                case < 0:
+                    throw new ArgumentException();
+                default:
+                    if (index < (this._queststep.Count() - 1))
+                    {
+                        this._questadvance = this._queststep[index + 1];
+                    }
+                    else if (index == (this._queststep.Count() - 1))
+                    {
+                        this._queststatus = "Finished";
+                        Console.WriteLine("You have finished a quest !");
+                    }
+                    break;
+            }
+
+        }
+    }
+
+}
